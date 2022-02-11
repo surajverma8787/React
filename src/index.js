87584b2361f399b056af8c42dc12b8eb211da75a
unknown <surajverma8787@gmail.com>
2022-02-11 20:30:26 +0530

    Intro

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-ReactDOM.render(<h1> Suraj Verma</h1>,
+import React from "react";
+import ReactDOM from "react-dom";
+const name = "Suraj";
+ReactDOM.render(
+    <div>
+        <h1>
+            My Favourite Food
+        </h1>
+        <ul>
+            <li>Bacon</li>
+            <li>Jamon</li>
+            <li>Noodles</li>
+        </ul>
+    </div>,
     document.getElementById("root")
 );
