d645490a92bff7ed7666f837e70a4cd5e72793a7
unknown <surajverma8787@gmail.com>
2022-02-12 16:02:36 +0530

    Import Export Modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
+//import { add, multiply, subtract, divide } from "./calculator.jsx";
+import * as Operations from "./calculator.jsx";
 ReactDOM.render(
-    <App />
+    <div>
+        <li>
+            {Operations.add(1, 2)}
+        </li>
+        <li>
+            {Operations.multiply(1, 2)}
+        </li>
+        <li>
+            {Operations.subtract(1, 2)}
+        </li>
+        <li>
+            {Operations.divide(1, 2)}
+        </li>
+    </div>
     , document.getElementById("root"));
