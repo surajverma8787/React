5d470a47d76cbc8969a579dae93c7086b0cb2def
unknown <surajverma8787@gmail.com>
2022-02-12 14:21:26 +0530

    React Components

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const date = new Date();
-const time = date.getHours();
-let text = "";
-const customStyle = {
-    color: "red"
-}
-if (time < 12) {
-    text = text + "Good Moring";
-    customStyle.color = "blue";
-}
-else if (time < 18) {
-    text = text + "Good Afternoon";
-    customStyle.color = "yellow";
-}
-else {
-    text = text + "Good Evening";
-    customStyle.color = "black";
-}
+import App from "./components/App.jsx";
 ReactDOM.render(
-    <div style={customStyle}>
-        <h1>{text}</h1>
-    </div>
+    <App />
     , document.getElementById("root"));
