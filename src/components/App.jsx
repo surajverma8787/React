498ad110edeaee19f1e650a03b602245d96eff20
unknown <surajverma8787@gmail.com>
2022-02-13 23:24:20 +0530

    Mapping Data to Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,21 @@ import Note from "./Note.jsx";
 import Images from "./Images.jsx";
 import contacts from "../contacts.js";
 import Avatar from "./Avatar";
+function createContact(contact) {
+    return <Images
+        key={contact.id}
+        name={contact.name}
+        image={contact.image}
+        mobile={contact.mobile}
+        email={contact.email}
+    />
+}
 function App() {
     return <div>
         <h1 className="heading">My Contacts</h1>
         <Avatar img="https://userpic.codeforces.org/1419937/title/d511c9794da58894.jpg" />
-        <Images
+        {contacts.map(createContact)};
+        {/* <Images
             name={contacts[0].name}
             image={contacts[0].image}
             mobile={contacts[0].mobile}
@@ -26,7 +36,7 @@ function App() {
             image={contacts[2].image}
             mobile={contacts[2].mobile}
             email={contacts[2].email}
-        />
+        /> */}
     </div>
 
 }
