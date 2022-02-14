de8ab6c8ae3e165520190b2155085cfe2539914c
unknown <surajverma8787@gmail.com>
2022-02-15 00:21:03 +0530

    React Conditional Rendering with Ternary Op

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import emojipedia from "../emojipedia.js";
-import Card from "./Card.jsx";
+import Login from "./Login.jsx";
+var isLoggedin = true;
+const obj = new Date();
+const currDay = obj.getDay();
 function App() {
     return (
-        <div>
-            <h1>
-                <span>emojipedia</span>
-            </h1>
-            <dl className="dictionary">
-                {emojipedia.map((emojipedia) =>
-                    <Card
-                        emoji={emojipedia.emoji} name={emojipedia.name} meaning={emojipedia.meaning}
-                    />
-                )};
-            </dl>
+        <div className="container">
+            {
+                isLoggedin === true ? <h1>Hello User</h1> : <Login />
+            }
+            {
+                currDay === 7 && <h1>Lets Enjoy</h1>
+            }
+
         </div>
     );
 }
