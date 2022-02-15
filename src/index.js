dd4d82ae6ad46943ffcd6569d0b4e426f4e74c42
unknown <surajverma8787@gmail.com>
2022-02-15 23:39:37 +0530

    Destructing Object and Array Challenge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-var count = 0;
+import cars from "./practice.js";
+const [honda, tesla] = cars;
+const { speedStats: { topSpeed: teslaTopSpeed } } = tesla;
+const { speedStats: { topSpeed: hondaTopSpeed } } = honda;
+const { coloursByPopularity: [hondaTopColour] } = honda;
+const { coloursByPopularity: [teslaTopColour] } = tesla;
+
 ReactDOM.render(
-    <App />,
+    <table>
+        <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+        </tr>
+        <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+        </tr>
+    </table>,
     document.getElementById("root")
 );
