459a71f1aede66c5f1a228abccf69eda496dba88
unknown <surajverma8787@gmail.com>
2022-02-17 00:10:28 +0530

    Event Handling in React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 function App() {
-    setInterval(checkTime, 1000);
-    var t = new Date().toLocaleTimeString();
-    const [time, setTime] = React.useState(t);
-    function checkTime() {
-        t = new Date().toLocaleTimeString();
-        setTime(t);
+    const [headingText, setHeadingText] = React.useState("Hello");
+    var [isMouseOver, setMouseOver] = React.useState(false);
+    function handleClick() {
+        setHeadingText("Submitted");
+    }
+    function onMouse() {
+        isMouseOver = true;
+        setMouseOver(isMouseOver);
+    }
+    function outMouse() {
+        isMouseOver = false;
+        setMouseOver(isMouseOver);
     }
     return (
         <div className="container">
-            <h1>{time}</h1>
-            <button onClick={checkTime}>Get Time</button>
-        </div>
+            <h1>{headingText}</h1>
+            <input type="text" placeholder="What's your name?" />
+            <button style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+                onClick={handleClick}
+                onMouseOver={onMouse}
+                onMouseOut={outMouse} >Submit</button>
+        </div >
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import cars from "./practice.js";
-const [honda, tesla] = cars;
-const { speedStats: { topSpeed: teslaTopSpeed } } = tesla;
-const { speedStats: { topSpeed: hondaTopSpeed } } = honda;
-const { coloursByPopularity: [hondaTopColour] } = honda;
-const { coloursByPopularity: [teslaTopColour] } = tesla;
+import App from "./components/App";
 
-ReactDOM.render(
-    <table>
-        <tr>
-            <th>Brand</th>
-            <th>Top Speed</th>
-        </tr>
-        <tr>
-            <td>{tesla.model}</td>
-            <td>{teslaTopSpeed}</td>
-            <td>{teslaTopColour}</td>
-        </tr>
-        <tr>
-            <td>{honda.model}</td>
-            <td>{hondaTopSpeed}</td>
-            <td>{hondaTopColour}</td>
-        </tr>
-    </table>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
